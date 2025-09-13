e0b4e5dad3a9014124ea9b22ac30d0d38910fd83
rishi-gowda-2004 <rishigowdaa.ec22@rvce.edu.in>
2025-09-13 15:22:59 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -29,14 +29,18 @@ reg wr_en ;
 
   // Replace tt_um_example with your module name:
   tt_um_tx_fsm user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+    `ifdef GL_TEST
+    .VPWR(VPWR),
+    .VGND(VGND),
+`endif
+    .ui_in  (ui_in),
+    .uo_out (uo_out),
+    .uio_in (uio_in),
+    .uio_out(uio_out),
+    .uio_oe (uio_oe),
+    .ena    (ena),
+    .clk    (clk),
+    .rst_n  (rst_n)   // active-low reset
   );
    initial begin
     clk = 0;
