742effa508bf497d040eea9e30ab98545a8cb7cd
rishi-gowda-2004 <rishigowdaa.ec22@rvce.edu.in>
2025-09-13 14:59:56 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,19 +1,8 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
-
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
-
-  // Wire up the inputs and outputs:
+   
   reg clk;
   reg rst_n;
   reg ena;
@@ -22,9 +11,22 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+  reg ena;
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
 
+   
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
+
+  // Wire up the inputs and outputs:
+
+   
   // Replace tt_um_example with your module name:
-  tt_um_fa_fa1 (
+  tt_um_tx_fsm user_project (
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
